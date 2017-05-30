a8a6001d1fec0522664f7fa2a101bd9e20e7d734
Kevin Gin <kevinagin@gmail.com>
2017-05-30 14:33:04 -0700

    (combinatorics) added nthPermutation and factorialRepresentation

--- src/combinatorics.js
+++ src/combinatorics.js
@@ -1,7 +1,15 @@
 module.exports = {
+  combinations,
+  _combinations,
+  choose,
+  nthPermutation
+}
+
+module.exports = {
   combinations: combinations,
   _combinations: _combinations,
-  choose: choose
+  choose: choose,
+  nthPermutation:
 }
 
 // applies callback to each k-sized combination in array
@@ -34,4 +42,143 @@ function choose(n,k) {
   var productRange = (a,b,result=1) => a === b ? result*a : productRange(a+1,b,result*a); 
   if (n <= 0  || k <= 0 || n < k) return 0;
   return productRange(n-k+1,n) / productRange(1,k);
+}
+
+// facotial, 0! = 1
+function factorial(n) {
+  if (n > 18) console.log('WARNING: results exceed Number.MAX_SAFE_INTEGER')
+  if (n <=1) return 1;
+  var result = 1;
+  for (var i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+
+}
+
+
+// finds nthPermutation, given current ordering
+// if you want nthPermutaiton sorted lexographically, e.g., you want to call on lexographically sorted array
+function nthPermutation(array,n) {
+  function insertSwap(i,j) {
+    var temp = array[i];
+    array[i] = array[j];
+    for (var index = i+1; index <= j; index++) {
+      nextTemp = array[index];
+      array[index] = temp;
+      temp = nextTemp;
+    }
+  }
+
+  var array = array.slice();  // copy array
+  var len = array.length;
+  if (n <= 0 || n > factorial(len)) {
+    console.log('out of range -- no such permutation');
+    return -1;
+  }
+
+  var remainder = n-1;  // minus 1, because first is when remainder = 0
+  var index = 0;
+
+  while (remainder > 0) {
+    divisor = factorial(len-index-1);  // minus 1, because no permutations above factorial(length)
+    var toSwap = index + Math.floor(remainder / divisor);
+    insertSwap(index,toSwap);
+    remainder = remainder % divisor;
+    index ++;
+  }
+  return array;
+}
+
+
+// used, e.g., in finding nthPermutation: https://en.wikipedia.org/wiki/Factorial_number_system
+function factorialRepresentation(n) {
+  var str = ''
+  var divisor = 1;
+  // var quotient = Math.floor(n / divisor++);
+  while (n > 0) {
+    // str = (n % divisor).toString() + str;
+    str = (n % divisor).toString() + str;
+
+    n = Math.floor(n / divisor);
+    divisor ++;
+  }
+  return str.split('').map(s => parseInt(s));
+}
+
+
+
+// Working on alternate nthPermutation that relies on factorialReprestation.
+// Initial implementation wasn't faster, but perhaps still could optimize.
+// function nthPermutationAlt(array,n) {
+//   // console.log(array)
+//   function insertSwap(a,b) {
+//     // console.log('called')
+//     var i = Math.min(a,b);
+//     var j = Math.max(a,b);
+
+//     var temp = array[i];
+//     array[i] = array[j];
+//     for (var index = i+1; index <= j; index++) {
+//       // console.log('swapping!')
+//       nextTemp = array[index];
+//       array[index] = temp;
+//       temp = nextTemp;
+//     }
+//   }
+
+//   var fRep = factorialRepresentation(n-1);  // n-1 because we start counting at zero
+
+//   var array = array.slice();  // copy array
+//   var len = array.length;
+//   if (n <= 0 || n > factorial(len)) {
+//     console.log('out of range -- no such permutation');
+//     return -1;
+//   }
+
+//   var startIndex = array.length - fRep.length
+//   // console.log(fRep)
+//   // console.log(array.length)
+//   // console.log(array[startIndex])
+//   for (var i = startIndex; i < array.length; i++) {
+//     console.log('swapping: ', i, fRep[i-startIndex]+i)
+//     insertSwap(i,fRep[i-startIndex]+i);
+//     // console.log(array);
+//   }
+
+//   return array;
+// }
+
+
+
+function nthPermutation(array,n) {
+  function insertSwap(i,j) {
+    var temp = array[i];
+    array[i] = array[j];
+    for (var index = i+1; index <= j; index++) {
+      nextTemp = array[index];
+      array[index] = temp;
+      temp = nextTemp;
+    }
+  }
+
+  var array = array.slice();  // copy array
+  var len = array.length;
+  if (n <= 0 || n > factorial(len)) {
+    console.log('out of range -- no such permutation');
+    return -1;
+  }
+
+  var remainder = n-1;  // minus 1, because first is when remainder = 0
+  var index = 0;
+
+  while (remainder > 0) {
+    divisor = factorial(len-index-1);  // minus 1, because no permutations above factorial(length)
+    var toSwap = index + Math.floor(remainder / divisor);
+    // console.log('swapping: ', index, toSwap)
+    insertSwap(index,toSwap);
+    remainder = remainder % divisor;
+    index ++;
+  }
+  return array;
 }
